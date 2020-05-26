3a720e86961b13d747b552a65bd8ec242374dee7
Chris Benseler <cbenseler@gmail.com>
2020-05-26 14:13:15 -0300

    feat: create book

--- api.go
+++ api.go
@@ -1,36 +1,47 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
-type book struct {
+type Book struct {
 	ID    int    `json:"id"`
 	Title string `json:"title"`
 	Year  int    `json:"year"`
 }
 
+var books = []Book{
+	Book{Title: "The Black Swan", Year: 2010, ID: 1},
+	Book{Title: "Skin in the Game", Year: 2012, ID: 2},
+}
+
 func listBooksEndpoint(c *gin.Context) {
-
-	var books = []book{
-		book{Title: "The Black Swan", Year: 2010, ID: 1},
-		book{Title: "Skin in the Game", Year: 2012, ID: 2},
-	}
-
-	fmt.Println("First Length:", len(books))
 
 	c.JSON(http.StatusOK, books)
 }
 
+func createBookEndpoint(c *gin.Context) {
+	var newBook Book
+
+	if c.ShouldBind(&newBook) == nil {
+		books = append(books, newBook)
+		c.JSON(http.StatusCreated, newBook)
+	} else {
+		c.JSON(http.StatusBadRequest, gin.H{})
+	}
+
+}
+
 func main() {
+
 	router := gin.Default()
 
 	booksRoutes := router.Group("/books")
 	{
 		booksRoutes.GET("/", listBooksEndpoint)
+		booksRoutes.POST("/", createBookEndpoint)
 	}
 
 	router.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
